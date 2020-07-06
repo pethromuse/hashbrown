edcacca7588938ca412c09b0ae7b157a8a5ff60f
pethromuse <pethromuse@gmail.com>
2020-07-07 00:38:15 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
+import profileImage from './img/mypicture.jpg'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My React Appㅎㅎㅎ</h1>
+      <h2>이름 : 정대혁</h2>
+      <img src={profileImage} alt="profile-image"/>
     </div>
   );
 }
